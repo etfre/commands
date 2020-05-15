d9011778b9f767ef528f9b17002d80ba05a98f72
Evan Fredericksen <evfredericksen@gmail.com>
2020-05-15 14:31:40 -0400

    wsl fixes

--- terminal/wsl.py
+++ terminal/wsl.py
@@ -4,10 +4,10 @@ import time
 import os
 import tempfile
 
-def drop(num):
+def drop(num, mount_root='/mnt'):
     num = int(num)
-    _, temp_name = tempfile.mkstemp()
-    read_name = '/mnt/c' + temp_name.replace('\\', '/')[2:]
+    fd, temp_name = tempfile.mkstemp()
+    read_name = f'{mount_root}/c' + temp_name.replace('\\', '/')[2:]
     keyboard.KeyPress.from_raw_text(f'ls > {read_name}').send()
     keyboard.KeyPress.from_space_delimited_string('enter').send()
     time.sleep(.5)
@@ -16,9 +16,10 @@ def drop(num):
             for i, line in enumerate(f, start=1):
                 if i == num:
                     line = line.rstrip("\n")
-                    keyboard.KeyPress.from_raw_text(f'cd {line}').send()
+                    keyboard.KeyPress.from_raw_text(f'cd "{line}"').send()
                     keyboard.KeyPress.from_space_delimited_string('enter').send()
                     break
     finally:
         if os.path.isfile(temp_name):
+            os.close(fd)
             os.remove(temp_name)
