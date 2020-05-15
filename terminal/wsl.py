11783e388a011fe2a1a5b7e5c1366ce5560ecafe
Evan Fredericksen <evfredericksen@gmail.com>
2020-05-15 14:47:58 -0400

    finer grained wsl.drop() timing

--- terminal/wsl.py
+++ terminal/wsl.py
@@ -5,20 +5,23 @@ import os
 import tempfile
 
 def drop(num, mount_root='/mnt'):
-    num = int(num)
     fd, temp_name = tempfile.mkstemp()
     read_name = f'{mount_root}/c' + temp_name.replace('\\', '/')[2:]
     keyboard.KeyPress.from_raw_text(f'ls > {read_name}').send()
     keyboard.KeyPress.from_space_delimited_string('enter').send()
-    time.sleep(.5)
+    num = int(num)
     try:
-        with open(temp_name) as f:
-            for i, line in enumerate(f, start=1):
-                if i == num:
-                    line = line.rstrip("\n")
-                    keyboard.KeyPress.from_raw_text(f'cd "{line}"').send()
-                    keyboard.KeyPress.from_space_delimited_string('enter').send()
-                    break
+        for i in range(200):
+            if os.stat(temp_name).st_size > 0:
+                with open(temp_name) as f:
+                    for i, line in enumerate(f, start=1):
+                        if i == num:
+                            line = line.rstrip("\n")
+                            keyboard.KeyPress.from_raw_text(f'cd "{line}"').send()
+                            keyboard.KeyPress.from_space_delimited_string('enter').send()
+                            break
+                break
+            time.sleep(0.01)
     finally:
         if os.path.isfile(temp_name):
             os.close(fd)
